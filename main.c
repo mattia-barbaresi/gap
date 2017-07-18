19c94f50c1cc62dc6f0c09f028cd55a1c4dde727
Massimo Neri <hello@mneri.me>
2017-07-18 23:08:23 +0200

    Copyright notices

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+ * This file is part of gap.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * under the terms of the GNU General Public License as published by
+ * Free Software Foundation, either version 3 of the License, or
+ * your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * ng with gap.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
